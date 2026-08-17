fd4857c683db3a1b3a3f96470ff4eac408e54268
marcela-mendoza <mendoza.marcelap@gmail.com>
2026-08-17 13:59:00 -0600

    cleaning up first round

--- scripts_nm/00_formattingAirPTfiles_NM.R
+++ scripts_nm/00_formattingAirPTfiles_NM.R
@@ -1,49 +1,71 @@
 ##==============================================================================
-## Project: QuEST
+## Project: FOR-NM 
 ## Script to format Air Pressure Transducers and upload them back to Drive
 ## press Command+Option+O to collapse all sections and get an overview of the workflow!
+##AirPTs for FOR-NMs are HOBO brand
 ##==============================================================================
 ##############
 ## Packages ##
 ##############
-library(googledrive)
+#library(googledrive)
 library(tidyverse)
 
 ########################################
 #### Clear folders that we will use ####
 ########################################
 # list and delete all files in the folder
-files <- list.files(path = "googledrive", full.names = TRUE)
-file.remove(files)
+#files <- list.files(path = "googledrive", full.names = TRUE)
+#file.remove(files)
 
 #####################
 #### Import Data ####
 #####################
 # set up Google Drive folder
 # this is the "raw air" folder
-pt <- googledrive::as_id("https://drive.google.com/drive/folders/1jhw-miI7fPaP8947HCbJ1idnIl6g1A8c")
+#pt <- googledrive::as_id("https://drive.google.com/drive/folders/1jhw-miI7fPaP8947HCbJ1idnIl6g1A8c")
 
 # list and filter CSV files with "pt" in their names
-pt_files <- googledrive::drive_ls(path = pt, type = "csv")
-3
+#pt_files <- googledrive::drive_ls(path = pt, type = "csv")
+
+#local path here 
+pt<- "data/AirPT/raw/"
+pt_files<- list.files(path = pt, pattern = "\\.csv$")
+
+output_path<- "data/AirPT/formatted/"
+dir.create(output_path)
+
+
 
 # create an empty list to store the cleaned data frames
-pt_list <- lapply(seq_along(pt_files$name), function(i) {
-  googledrive::drive_download(
-    file = pt_files$id[i],
-    path = paste0("googledrive/", pt_files$name[i]),
-    overwrite = TRUE
-  )
-  
+pt_list <- lapply(seq_along(pt_files), function(i) {
   # read the CSV file, skipping the first 11 rows (header is on row 12)
-  read.csv(paste0("googledrive/", pt_files$name[i]), skip = 10, header = TRUE)
+  read.csv(paste0(pt, pt_files[i]),skip=1,  header = TRUE)
 })
 
 # assign names to the list elements based on the file names
-names(pt_list) <- pt_files$name
+names(pt_list) <- pt_files
 
 # check the contents of the list
 str(pt_list)
+
+###################################
+#### Format names to match PT ####
+###################################
+for (i in seq_along(pt_list)) {
+  # Access the current data frame
+  df <- pt_list[[i]]
+  
+  # Rename columns
+  df <- df %>%
+    dplyr::rename(DateTime = Date.Time..GMT.07.00)
+  
+  # Rename columns to standard names based on prefixes
+  df <- df %>%
+    rename_with(~ "TEMPERATURE", .cols = starts_with("Temp")) %>%
+    rename_with(~ "LEVEL", .cols = starts_with("Abs.Pres"))
+  # Save back to the list
+  pt_list[[i]] <- df
+}
 
 ############################
 #### Format date column ####
@@ -53,32 +75,13 @@ for (i in seq_along(pt_list)) {
   # Access the current data frame
   df <- pt_list[[i]]
   
-  # make date into date fomat
-  df$Date <- as.Date(df$Date, format = "%m/%d/%Y")
-  # update the data frame in the list
+  # Convert the DateTime column to POSIXct
+  df$DateTime <-  as.POSIXct(df$DateTime, format = "%m/%d/%y %I:%M:%S %p")
+  # Update the data frame in the list
   pt_list[[i]] <- df
+  
 }
 
-##########################################################
-#### Combine and format Date and Time into one column #### 
-##########################################################
-# loop through each data frame in the list
-for (i in seq_along(pt_list)) {
-  # Access the current data frame
-  df <- pt_list[[i]]
-  # combine Date and Time columns into a new DateTime column
-  df$DateTime <- paste(df$Date, df$Time, sep = " ")
-  
-  # convert the DateTime column to POSIXct
-  df$DateTime <- as.POSIXct(df$DateTime, format = "%Y-%m-%d %I:%M:%S %p")
-  # update the data frame in the list
-  pt_list[[i]] <- df
-  
-  # make date into date fomat
-  df$Date <- as.Date(df$Date, format = "%Y-%m-%d")
-  # update the data frame in the list
-  pt_list[[i]] <- df
-}
 
 # check the contents of the list and make sure there are no NAs
 str(pt_list)
@@ -92,17 +95,7 @@ for (i in seq_along(pt_list)) {
   df <- pt_list[[i]]
   
   # save new data frame
-  write.csv(df, paste0("googledrive/", pt_files$name[i]), row.names=FALSE, quote=FALSE)
+  write.csv(df, paste0(output_path, pt_files[i]), row.names=FALSE, quote=FALSE)
   
-  # define the local folder path and the target folder ID in Google Drive
-  file <- paste0("googledrive/", pt_files$name[i])
-  # this is the "in use" folder
-  drive_folder_id <- "1czHQWopN5_uw6wnU0m1hvjJuHI4D6ABP"
-  
-  # upload file to the specified Google Drive folder
-  drive_put(
-    media = file,
-    path = as_id(drive_folder_id)
-  )
 }
 
--- scripts_nm/00_formattingPTfiles_NM.R
+++ scripts_nm/00_formattingPTfiles_NM.R
@@ -1,63 +1,54 @@
 ##==============================================================================
-## Project: QuEST
+## Project: FOR-NM 
 ## Script to format all the Pressure Transducer (Level Logger) to a cleaner state and upload them back to Drive
 ## press Command+Option+O to collapse all sections and get an overview of the workflow!
+## FOR-NM PTs are Solonist brand 
 ##==============================================================================
 ##############
 ## Packages ##
 ##############
-library(googledrive)
+#library(googledrive)
 library(tidyverse)
 
 ########################################
 #### Clear folders that we will use ####
 ########################################
 # list and delete all files in the folder
-files <- list.files(path = "googledrive", full.names = TRUE)
-file.remove(files)
+#files <- list.files(path = "googledrive", full.names = TRUE)
+#file.remove(files)
 
 #####################
 #### Import Data ####
 #####################
 # set up Google Drive folder
-# this is the "inuse" folder
-pt <- googledrive::as_id("https://drive.google.com/drive/folders/1i7G-q7FV0_bszqeCdJ6Otz8b9xhpU1cx")
+# this is the "raw air" folder
+#pt <- googledrive::as_id("https://drive.google.com/drive/folders/1jhw-miI7fPaP8947HCbJ1idnIl6g1A8c")
 
 # list and filter CSV files with "pt" in their names
-pt_files <- googledrive::drive_ls(path = pt, type = "csv")
-3
+#pt_files <- googledrive::drive_ls(path = pt, type = "csv")
+
+#local path here 
+pt<- "data/PT/raw/"
+pt_files<- list.files(path = pt, pattern = "\\.csv$")
+
+output_path<- "data/PT/formatted/"
+dir.create(output_path)
+
+
 
 # create an empty list to store the cleaned data frames
-pt_list <- lapply(seq_along(pt_files$name), function(i) {
-  googledrive::drive_download(
-    file = pt_files$id[i],
-    path = paste0("googledrive/", pt_files$name[i]),
-    overwrite = TRUE
-  )
-  
+pt_list <- lapply(seq_along(pt_files), function(i) {
   # read the CSV file, skipping the first 11 rows (header is on row 12)
-  read.csv(paste0("googledrive/", pt_files$name[i]), skip = 11, header = TRUE)
+  read.csv(paste0(pt, pt_files[i]),skip=13,  header = TRUE)
 })
 
 # assign names to the list elements based on the file names
-names(pt_list) <- pt_files$name
+names(pt_list) <- pt_files
 
 # check the contents of the list
 str(pt_list)
 
-############################
-#### Format date column ####
-############################
-# loop through each data frame in the list
-for (i in seq_along(pt_list)) {
-  # Access the current data frame
-  df <- pt_list[[i]]
-  
-  # make date into date fomat
-  df$Date <- as.Date(df$Date, format = "%m/%d/%Y")
-  # update the data frame in the list
-  pt_list[[i]] <- df
-}
+
 
 ##########################################################
 #### Combine and format Date and Time into one column #### 
@@ -66,16 +57,16 @@ for (i in seq_along(pt_list)) {
 for (i in seq_along(pt_list)) {
   # Access the current data frame
   df <- pt_list[[i]]
+  
+  # make date into date fomat
+  df$Date <- as.Date(df$Date, format = "%m/%d/%Y")
   # combine Date and Time columns into a new DateTime column
   df$DateTime <- paste(df$Date, df$Time, sep = " ")
   
   # convert the DateTime column to POSIXct
   df$DateTime <- as.POSIXct(df$DateTime, format = "%Y-%m-%d %I:%M:%S %p")
-  # update the data frame in the list
-  pt_list[[i]] <- df
   
-  # make date into date fomat
-  df$Date <- as.Date(df$Date, format = "%Y-%m-%d")
+  
   # update the data frame in the list
   pt_list[[i]] <- df
 }
@@ -84,7 +75,7 @@ for (i in seq_along(pt_list)) {
 str(pt_list)
 
 #########################################
-#### Save edited files back to Drive ####
+#### Save edited files back to  ####
 #########################################
 # loop through each data frame in the list
 for (i in seq_along(pt_list)) {
@@ -92,17 +83,7 @@ for (i in seq_along(pt_list)) {
   df <- pt_list[[i]]
   
   # save new data frame
-  write.csv(df, paste0("googledrive/", pt_files$name[i]), row.names=FALSE, quote=FALSE)
+  write.csv(df, paste0(output_path, pt_files[i]), row.names=FALSE, quote=FALSE)
   
-  # define the local folder path and the target folder ID in Google Drive
-  file <- paste0("googledrive/", pt_files$name[i])
-  # this is the "in use" folder
-  drive_folder_id <- "1i7G-q7FV0_bszqeCdJ6Otz8b9xhpU1cx"
-  
-  # upload file to the specified Google Drive folder
-  drive_put(
-    media = file,
-    path = as_id(drive_folder_id)
-  )
 }
 
